6195e5e0f4f8de18fc5cd6452c6fe580c1e839f6
jacketsj <jacketsj@gmail.com>
2024-04-18 01:12:16 -0700

    Closer to functioning implementation

--- ann_benchmarks/algorithms/module.py
+++ ann_benchmarks/algorithms/module.py
@@ -6,31 +6,38 @@ from ..base.module import BaseANN
 print(expann_py)
 
 class ExpAnnWrapper(BaseANN):
-    def __init__(self, metric, index_params):
+    # TODO what are the params to this?? args?
+    def __init__(self, metric, index_param, query_param):
+        # TODO: Actually use the params?
         self.engine = expann_py.expANN()
         self.name = self.engine.name()
         self.res = None
+        self.metric = metric
 
     def fit(self, X):
+        dim = X.shape[1]
         for vector in X:
             v = expann_py.Vec(vector.tolist())
             self.engine.store_vector(v)
         self.engine.build()
 
-    def query(self, X, k):
-        query_vectors = []
-        for query_vector in X:
-            v = expann_py.Vec(query_vector.tolist())
-            query_vectors.append(v)
-        result_indices = self.engine.query_k_batch(query_vectors, k)
+    def query(self, q, k):
+        q = expann_py.Vec(q.to_list())
+        return self.engine.query_k(q, k)
+        #query_vectors = []
+        #for query_vector in Q:
+        #    q = expann_py.Vec(query_vector.tolist())
+        #    query_vectors.append(q)
+        #result_indices = self.engine.query_k_batch(query_vectors, k)
 
-        max_len = len(max(result_indices, key=len))
-        for result_index in result_indices:
-            while len(result_index) < max_len:
-                result_index.append(0)
-        self.res = np.array(result_indices)
+        #max_len = len(max(result_indices, key=len))
+        #for result_index in result_indices:
+        #    while len(result_index) < max_len:
+        #        result_index.append(0)
+        #self.res = np.array(result_indices)
 
     def set_query_arguments(self, query_args):
+        # TODO: Actually do this?
         pass
 
     def load_index(self, dataset):
